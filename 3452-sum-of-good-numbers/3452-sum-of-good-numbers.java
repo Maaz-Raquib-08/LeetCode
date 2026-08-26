135bd9143c7da35bb6c7c51cc9f279acf9edc18b
Maaz-Raquib-08 <maazraquib37@gmail.com>
2026-08-26 17:44:08 +0530

    Time: 1 ms (99.39%), Space: 45.4 MB (38.57%) - LeetHub

---
+++ 3452-sum-of-good-numbers/3452-sum-of-good-numbers.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int sumOfGoodNumbers(int[] nums, int k) {
+        int sum = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if ((i - k < 0) && (i + k >= n)) {
+                sum += nums[i];
+            } else if (i - k < 0) {
+                if (nums[i] > nums[i + k]) {
+                    sum += nums[i];
+                }
+            } else if (i + k >= n) {
+                if (nums[i] > nums[i - k]) {
+                    sum += nums[i];
+                }
+            } else if ((nums[i] > nums[i - k]) && (nums[i] > nums[i + k])) {
+                sum += nums[i];
+            }
+        }
+        return sum;
+    }
+}
