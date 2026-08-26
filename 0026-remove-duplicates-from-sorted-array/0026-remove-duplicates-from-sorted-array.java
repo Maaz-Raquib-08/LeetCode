c40551462c5774cf1df5fc04edfc13c5c05840e2
Maaz-Raquib-08 <maazraquib37@gmail.com>
2026-08-26 17:48:07 +0530

    Time: 0 ms (100%), Space: 46.6 MB (82.1%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n=0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]!=nums[n]){
+                n++;
+                nums[n]=nums[i];
+            }   
+        
+        }
+        return n+1;
+    }
+}
