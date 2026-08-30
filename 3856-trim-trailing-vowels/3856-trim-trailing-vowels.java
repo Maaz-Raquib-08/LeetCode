a7878abd7868bda72262fcfb6356c475a33d0f6b
Maaz-Raquib-08 <maazraquib37@gmail.com>
2026-08-30 14:57:02 +0530

    Time: 1 ms (99.03%), Space: 44.5 MB (35.55%) - LeetHub

---
+++ 3856-trim-trailing-vowels/3856-trim-trailing-vowels.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String trimTrailingVowels(String s) {
+
+        int count=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)!= 'a' && s.charAt(i) != 'e' &&s.charAt(i) != 'i' && s.charAt(i) != 'o' && s.charAt(i)  != 'u'){
+                break;
+            }
+            count++;
+        }
+      return s.substring(0,s.length()-count);
+    }
+}
