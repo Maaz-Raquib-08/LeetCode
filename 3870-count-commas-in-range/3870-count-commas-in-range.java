51dd91304c5adf3baaa4f338fbf327d5daffd44f
Maaz-Raquib-08 <maazraquib37@gmail.com>
2026-09-08 15:28:11 +0530

    Time: 1 ms (83.65%), Space: 42.7 MB (39.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,12 +1,16 @@
 class Solution {
     public int countCommas(int n) {
-        int m=0;
-        if(999>=n){
+        // int m=0;
+        // if(999>=n){
+        //     return 0;
+        // }
+        // else{
+        //      m=n-999;
+        // }
+        // return m;
+        if(n<1000){
             return 0;
         }
-        else{
-             m=n-999;
-        }
-        return m;
+        return n-999;
     }
 }
