5ac04b7290755a875a5e50a2276463925d87fb68
Maaz-Raquib-08 <maazraquib37@gmail.com>
2026-09-19 07:37:12 +0530

    Time: 2 ms (66.35%), Space: 43 MB (71.28%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,13 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+       int sums=0;
+       int sumt=0;
+       for(int i=0;i<s.length();i++){
+        sums+=s.charAt(i);
+       }
+       for(int j=0;j<t.length();j++){
+        sumt+=t.charAt(j);
+       }
+       return (char)(sumt-sums);
+    }
+}
